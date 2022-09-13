1ee042b5eba2cfb3f0014b4eefd0ec9422eb8170
Lui H <tech.luiholl@gmail.com>
2022-09-13 16:49:25 +0200

    changed button className to "vote"

--- src/components/DogHeadToHead.tsx
+++ src/components/DogHeadToHead.tsx
@@ -50,7 +50,7 @@ export default function DogHeadToHead(): JSX.Element {
       />
       <button
         onClick={() => handleVoteForDog(dogComparer[0].breed)}
-        className="voteDog"
+        className="vote"
       >
         vote for {dogComparer[0].breed}
       </button>
@@ -61,7 +61,7 @@ export default function DogHeadToHead(): JSX.Element {
       />
       <button
         onClick={() => handleVoteForDog(dogComparer[1].breed)}
-        className="voteDog"
+        className="vote"
       >
         vote for {dogComparer[1].breed}
       </button>
