0b58b492a9aaf13991c7f90758f3028d1938ca6d
Salman0112358 <Salman0112358@outlook.com>
2022-09-15 17:51:43 +0100

    Added logic switch to go live or local based on environment

--- src/utils/serverUrl.ts
+++ src/utils/serverUrl.ts
@@ -1,3 +1,7 @@
-const serverUrl = "https://c5b3-dog-voting-app.herokuapp.com";
 
+const onNetlify = process.env.NODE_ENV === "production"
+
+const serverUrl = (onNetlify ? 
+    "https://c5b3-dog-voting-app.herokuapp.com" : 
+    "http://localhost:4000")
 export default serverUrl;
