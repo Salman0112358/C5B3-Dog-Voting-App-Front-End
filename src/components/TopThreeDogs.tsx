3ec1e287b00df0fc5ca103fc93cbb23a9bf02fc0
Salman0112358 <Salman0112358@outlook.com>
2022-09-16 13:37:26 +0100

    format run

--- src/components/TopThreeDogs.tsx
+++ src/components/TopThreeDogs.tsx
@@ -11,7 +11,9 @@ export default function TopThreeDogs({ topTenDogs }: IProps): JSX.Element {
       {topTenDogs.slice(0, 3).map((dog, index) => (
         <div key={dog.dog_id ?? index} className="top_three_labels">
           <p>
-            <b>{dog.breed} {dog.votes}</b>
+            <b>
+              {dog.breed} {dog.votes}
+            </b>
           </p>
           <img
             className="dog-image_top_three"
